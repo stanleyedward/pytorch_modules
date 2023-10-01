c6a283e492c214a33b424937e0a892879ce6b566
stanleyedward <stanleyedward9715@gmail.com>
2023-10-01 11:23:00 +0530

    minor chnage

--- modules/build_model.py
+++ modules/build_model.py
@@ -6,7 +6,7 @@ import torch
 from torch import nn
 
 class TinyVGG(nn.Module):
-    """creates tinyvgg
+    """creates tinyvgg instance 
     """
     def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
         super().__init__()
