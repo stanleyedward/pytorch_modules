04ac6dd934fa742a577e696ab9309d6e28d81b06
stanleyedward <stanleyedward9715@gmail.com>
2023-10-01 10:53:11 +0530

    add file

---
+++ modules/prediction.py
@@ -0,0 +1,81 @@
+"""
+functions to make predictions.
+
+refernce https://www.learnpytorch.io/06_pytorch_transfer_learning/#6-make-predictions-on-images-from-the-test-set 
+"""
+import torch
+import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+from typing import List, Tuple
+
+from PIL import Image
+
+# set device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# predict on a target image with a target model
+def pred_and_plot_image(
+    model: torch.nn.Module,
+    class_names: List[str],
+    image_path: str,
+    image_size: Tuple[int, int] = (224, 224),
+    transform: torchvision.transforms = None,
+    device: torch.device = device,
+):
+    """predicts target image 
+
+    Args:
+        model (torch.nn.Module): model instance.
+        class_names (List[str]): list[].
+        image_path (str): path.
+        image_size (Tuple[int, int], optional): size of image. defaults to (224, 224).
+        transform (torchvision.transforms, optional): transforms to perform, defults to imagemet transforms.
+        device (torch.device, optional): target device. defaults to device.
+    """
+
+    # open image
+    img = Image.open(image_path)
+
+    # create transformation for image (if one doesn't exist)
+    if transform is not None:
+        image_transform = transform
+    else:
+        image_transform = transforms.Compose(
+            [
+                transforms.Resize(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+
+    ### predict on image ###
+
+    # make sure the model is on the target device
+    model.to(device)
+
+    # turn on model evaluation mode and inference mode
+    model.eval()
+    with torch.inference_mode():
+        # transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+        transformed_image = image_transform(img).unsqueeze(dim=0)
+
+        # make a prediction on image with an extra dimension and send it to the target device
+        target_image_pred = model(transformed_image.to(device))
+
+    # convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    # convert prediction probabilities -> prediction labels
+    target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
+    # plot image with predicted label and probability
+    plt.figure()
+    plt.imshow(img)
+    plt.title(
+        f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}"
+    )
+    plt.axis(False)
