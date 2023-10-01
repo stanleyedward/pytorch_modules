8059d05f19463c1707a1c5c9dcf7333eeeff3c10
stanleyedward <stanleyedward9715@gmail.com>
2023-10-01 14:18:14 +0530

    minor fix

--- modules/train_engine.py
+++ modules/train_engine.py
@@ -6,7 +6,7 @@ from typing import Dict, List, Tuple
 import torch
 
 from tqdm.auto import tqdm
-
+from torch.utils.tensorboard import SummaryWriter
 def train_step(model: torch.nn.Module, 
                dataloader: torch.utils.data.DataLoader, 
                loss_fn: torch.nn.Module, 
